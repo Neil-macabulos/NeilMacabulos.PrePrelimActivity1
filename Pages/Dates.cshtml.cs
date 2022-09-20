f5360f8e9b6b73d5805e32f8e046617709785afc
NEIL R. MACABULOS <NEIL2003>
2022-09-20 13:36:16 +0800

    Prelim Activity1

---
+++ Pages/Dates.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NeilMacabulos.DateCalculator_.Pages
+{
+    public class DatesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
